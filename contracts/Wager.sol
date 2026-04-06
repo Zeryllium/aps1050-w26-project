c7a50166687981d474842d833a53b98f0d6574ec
Zeryllium <zeryllium@gmail.com>
2026-04-05 20:51:52 -0400

    Remove unused functions from `Wager.sol`

--- contracts/Wager.sol
+++ contracts/Wager.sol
@@ -52,16 +52,8 @@ contract Wager is Ownable {
         _;
     }
 
-    function getPendingMatches() public view oracleReady returns (uint32[]) {
-        return oracle.getMatchesWithStatus(OracleInterface.MatchStatus.Pending);
-    }
-
-    function getMatch(uint32 _matchId) public view oracleReady returns (OracleInterface.Match) {
-        return oracle.getMatchByID(_matchId);
-    }
-
     function placeBet(uint32 _matchId, uint32 _chosenTeamId, uint256 _amount) external oracleReady {
-        require(getMatch(_matchId).status == OracleInterface.MatchStatus.Pending, "Betting window has closed");
+        require(oracle.getMatchByID(_matchId).status == OracleInterface.MatchStatus.Pending, "Betting window has closed");
         // TODO: Is there reason to (allow / not allow) multiple bets from the same user on the same match?
         // require(!hasUserBetOnMatch[msg.sender][_matchId], "Cannot bet twice on the same match");
         require(lightToken.transferFrom(msg.sender, address(this), _amount), "Transaction failed");
@@ -78,7 +70,7 @@ contract Wager is Ownable {
 
     function claimEarnings(uint32 _matchId) external oracleReady {
         WagerMatch storage wagerMatch = matches[_matchId];
-        OracleInterface.Match memory oracleMatch = getMatch(_matchId);
+        OracleInterface.Match memory oracleMatch = oracle.getMatchByID(_matchId);
 
         require(oracleMatch.status == OracleInterface.MatchStatus.Finished, "Match is not over");
         require(!hasUserClaimedMatch[msg.sender][_matchId], "Already claimed earnings");
